52990db6e19770fdfeff37c45046f08dfb22298c
tayta21 <ahmedtritar21@gmail.com>
2020-08-17 05:31:51 +0100

    Updates

---
+++ precache-manifest.c51b7e1b0afb5b25a8d92435ef4c39e7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6786f6ad49f69fbffb052776d86726b9",
+    "url": "/TaytaHotels.com/index.html"
+  },
+  {
+    "revision": "dc559433c63c88517a64",
+    "url": "/TaytaHotels.com/static/css/2.fc5c5e36.chunk.css"
+  },
+  {
+    "revision": "dfc8398457415d38bb4b",
+    "url": "/TaytaHotels.com/static/css/main.4d3a8879.chunk.css"
+  },
+  {
+    "revision": "dc559433c63c88517a64",
+    "url": "/TaytaHotels.com/static/js/2.fe4c158a.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/TaytaHotels.com/static/js/2.fe4c158a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfc8398457415d38bb4b",
+    "url": "/TaytaHotels.com/static/js/main.8cd60b6b.chunk.js"
+  },
+  {
+    "revision": "a08b83112839e08b2161",
+    "url": "/TaytaHotels.com/static/js/runtime-main.51439b97.js"
+  },
+  {
+    "revision": "1e315b31324c25d93fd7650172645e01",
+    "url": "/TaytaHotels.com/static/media/background.1e315b31.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TaytaHotels.com/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/TaytaHotels.com/precache-manifest.c51b7e1b0afb5b25a8d92435ef4c39e7.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TaytaHotels.com/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
